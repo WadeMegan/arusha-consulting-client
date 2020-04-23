47832edf4cdbbfbafcf0dea5f98827749058837d
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-04-23 16:33:27 -0500

    updated styling

--- src/routes/FairgroundAssetsPage/FairgroundAssetsPage.js
+++ src/routes/FairgroundAssetsPage/FairgroundAssetsPage.js
@@ -7,39 +7,7 @@ import NavLinks from '../../components/NavLinks/NavLinks'
 export default class FairgroundAssetsPage extends Component{
     
     createCreditBadge =()=>{
-        
-        let aStyles={
-            backgroundColor:'black',
-            color:'white',
-            textDecoration:'none',
-            padding:'4px 6px',
-            fontFamily:'-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif',
-            fontSize:'12px',
-            fontWeight:'bold',
-            lineHeight:'1.2',
-            display:'inline-block',
-            borderRadius:'3px'
-        }
-
-        let spanStyles={
-            display:'inline-block',
-            padding:'2px 3px'
-        }
-
-        let svgStyles={
-            height:'12px',
-            width:'auto',
-            position:'relative',
-            verticalAlign:'middle',
-            top:'-2px',
-            fill:'white'
-        }
-
-        let span2Styles={
-            display:'inline-block',
-            padding:'2px 3px'
-        }
-
+    
         return (
             /*<a className='creditBadge' style={aStyles} href="https://unsplash.com/@llgp?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Laura Louise Grimsley"><span style={spanStyles}><svg xmlns="http://www.w3.org/2000/svg" style={svgStyles} viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style={span2Styles}>Laura Louise Grimsley</span></a>*/
             <a className='creditBadge' target='_blank' rel="noopener noreferrer" href='https://unsplash.com/@llgp'>Photo by Laura Louise Grimsley on Unsplash</a>
